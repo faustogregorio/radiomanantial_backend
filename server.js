52bc3fad49bb8570769262b983c5a677535973ce
Fausto Gregorio <fausto.g03@gmail.com>
2020-06-12 20:26:41 -0500

    cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const express = require("express");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const fileupload = require("express-fileupload");
@@ -29,7 +28,12 @@ const app = express();
 
 // Body parser
 app.use(express.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 // Cookie parser
 app.use(cookieParser());
 
