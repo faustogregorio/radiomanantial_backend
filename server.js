5df8e266fe7151da045b3f4de1456fbf5f10d175
Fausto Gregorio <fausto.g03@gmail.com>
2020-06-13 01:26:04 -0500

    .

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ const app = express();
 
 // Body parser
 app.use(express.json());
-app.all('*', function(req, res, next) {
+/* app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With, Origin');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
-});
+}); */
 /* app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, XMLHttpRequest");
